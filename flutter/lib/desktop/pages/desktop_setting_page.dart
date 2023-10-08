574f5b8b47d25a574fd2119602e716c95bc50bbc
dignow <linlong1265@gmail.com>
2023-10-08 22:32:48 +0800

    feat, multi flutter ui sessions, change settings to 'Display'
    Signed-off-by: dignow <linlong1265@gmail.com>

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -268,7 +268,6 @@ class _GeneralState extends State<_General> {
             service(),
             theme(),
             hwcodec(),
-            chooseDisplay(),
             audio(context),
             record(context),
             _Card(title: 'Language', children: [language()]),
@@ -357,29 +356,6 @@ class _GeneralState extends State<_General> {
         _OptionCheckBox(context, 'Enable hardware codec', 'enable-hwcodec'),
       ]),
     );
-  }
-
-  Widget chooseDisplay() {
-    if (!useTextureRender) return const Offstage();
-
-    var current = getChooseDisplayBehavior();
-    onChanged(String value) {
-      bind.mainSetOption(key: kKeyChooseDisplayBehavior, value: value);
-      setState(() {});
-    }
-
-    return _Card(title: 'Choose Display Behavior', children: [
-      _Radio<String>(context,
-          value: kChooseDisplayBehaviorSwitch,
-          groupValue: current,
-          label: 'Switch Display',
-          onChanged: onChanged),
-      _Radio<String>(context,
-          value: kChooseDisplayBehaviorOpen,
-          groupValue: current,
-          label: 'Open in New Window',
-          onChanged: onChanged),
-    ]);
   }
 
   Widget audio(BuildContext context) {
@@ -1134,12 +1110,36 @@ class _DisplayState extends State<_Display> {
             controller: scrollController,
             physics: DraggableNeverScrollableScrollPhysics(),
             children: [
+              chooseDisplay(context),
               viewStyle(context),
               scrollStyle(context),
               imageQuality(context),
               codec(context),
               other(context),
             ]).marginOnly(bottom: _kListViewBottomMargin));
+  }
+
+  Widget chooseDisplay(BuildContext context) {
+    if (!useTextureRender) return const Offstage();
+
+    var current = getChooseDisplayBehavior();
+    onChanged(String value) {
+      bind.mainSetOption(key: kKeyChooseDisplayBehavior, value: value);
+      setState(() {});
+    }
+
+    return _Card(title: 'Choose Display Behavior', children: [
+      _Radio<String>(context,
+          value: kChooseDisplayBehaviorSwitch,
+          groupValue: current,
+          label: 'Switch display',
+          onChanged: onChanged),
+      _Radio<String>(context,
+          value: kChooseDisplayBehaviorOpen,
+          groupValue: current,
+          label: 'Open in new window',
+          onChanged: onChanged),
+    ]);
   }
 
   Widget viewStyle(BuildContext context) {
