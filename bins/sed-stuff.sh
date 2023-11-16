da55a7a9dcc46d0bda8abbc259cb2d9ad8b99b1f
deep-soft <deep-soft@users.noreply.github.com>
2023-11-16 23:48:28 +0200

    Update sed-stuff.sh

--- bins/sed-stuff.sh
+++ bins/sed-stuff.sh
@@ -12,11 +12,14 @@ fi
 
 if [[ -f "$file_in" ]]; then
   echo "file_in:[$file_in]"
-  cat $file_in | while read the_line
+  cat $file_in | while read -r the_line
   do
-    if [[ ${the_line:0:2} == "S:" ]]; then _str_=${the_line:2}; echo "str:[$_str_]"; fi
-    if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=${the_line:2}; echo "rpl:[$_rpl_]"; fi
-    if [[ ${the_line:0:2} == "F:" ]]; then _fil_=${the_line:2}; echo "fil:[$_fil_]"; fi
+    #if [[ ${the_line:0:2} == "S:" ]]; then _str_=$(printf "%s" "${the_line:2}" | tr -d "\r"); echo "str:[$_str_]"; fi
+    #if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=${the_line:2}; echo "rpl:[$_rpl_]"; fi
+    #if [[ ${the_line:0:2} == "F:" ]]; then _fil_=${the_line:2}; echo "fil:[$_fil_]"; fi
+    if [[ ${the_line:0:2} == "S:" ]]; then _str_=$(echo "${the_line:2}" | tr -d "\n"); echo "str:[$_rpl_]"; fi
+    if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=$(echo "${the_line:2}" | tr -d "\n"); echo "rpl:[$_rpl_]"; fi
+    if [[ ${the_line:0:2} == "F:" ]]; then _fil_=$(echo "${the_line:2}" | tr -d "\n"); echo "fil:[$_rpl_]"; fi
     if [[ "$_fil_" != "" ]]; then
       if [[ -f "$_fil_" ]]; then
         echo "sed -i \"s|$_str_|$_rpl_|\" $_fil_";
