a3162e3f5dee7014935c2852bf1ac3d1267fc9a3
deep-soft <deep-soft@users.noreply.github.com>
2023-11-16 23:04:13 +0200

    Update sed-stuff.sh

--- bins/sed-stuff.sh
+++ bins/sed-stuff.sh
@@ -14,9 +14,9 @@ if [[ -f "$file_in" ]]; then
   echo "file_in:[$file_in]"
   cat $file_in | while read the_line
   do
-    if [[ ${the_line:0:2} == "S:" ]]; then _str_=${the_line:2}; echo "str:[$_str_]" fi
-    if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=${the_line:2}; echo "rpl:[$_rpl_]" fi
-    if [[ ${the_line:0:2} == "F:" ]]; then _fil_=${the_line:2}; echo "fil:[$_fil_]" fi
+    if [[ ${the_line:0:2} == "S:" ]]; then _str_=${the_line:2}; echo "str:[$_str_]"; fi
+    if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=${the_line:2}; echo "rpl:[$_rpl_]"; fi
+    if [[ ${the_line:0:2} == "F:" ]]; then _fil_=${the_line:2}; echo "fil:[$_fil_]"; fi
     if [[ "$_fil_" != "" ]]; then
       if [[ -f "$_fil_" ]]; then
         echo "sed -i \"s|$_str_|$_rpl_|\" $_fil_";
