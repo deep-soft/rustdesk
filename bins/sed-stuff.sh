4afd630a520f95dec90214004f4f99853a3e0b55
deep-soft <deep-soft@users.noreply.github.com>
2023-11-19 08:42:18 +0200

    Update sed-stuff.sh

--- bins/sed-stuff.sh
+++ bins/sed-stuff.sh
@@ -19,9 +19,11 @@ else
 fi
 
 if [[ -f "$file_in" ]]; then
+  count=0
   echo "file_in:[$file_in]"
   cat $file_in | while read -r the_line
   do
+    count=$count+1
     if [[ ${the_line:0:2} == "S:" ]]; then _str_=$(echo "${the_line:2}" | tr -d "\r"); echo_debug "str:[$_str_]"; fi
     if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=$(echo "${the_line:2}" | tr -d "\r"); echo_debug "rpl:[$_rpl_]"; fi
     if [[ ${the_line:0:2} == "F:" ]]; then _fil_=$(echo "${the_line:2}" | tr -d "\r"); echo_debug "fil:[$_fil_]"; fi
@@ -29,6 +31,12 @@ if [[ -f "$file_in" ]]; then
       if [[ -f "$_fil_" ]]; then
         echo_debug "sed -i \"s|$_str_|$_rpl_|\" $_fil_";
         sed -i "s|$_str_|$_rpl_|" $_fil_;
+        _str_n_=$(grep "$_str_" "$_fil_");
+        if [[ "$_str_n_" == "" ]]; then
+          echo "OK: $count";
+        else
+          echo "NOTOK: $count";
+        fi
         if [[ $debug_mode == "Y" ]]; then
           echo "grep 1"
           grep "$_str_" "$_fil_";
@@ -43,4 +51,5 @@ if [[ -f "$file_in" ]]; then
       _fil_="";
     fi
   done
+  echo "count:[$count]"
 fi
