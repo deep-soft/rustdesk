63a190364f791322ddd77afd6b77e62d25d69026
deep-soft <deep-soft@users.noreply.github.com>
2023-11-19 09:59:17 +0200

    Update sed-stuff.sh

--- bins/sed-stuff.sh
+++ bins/sed-stuff.sh
@@ -7,7 +7,7 @@ debug_mode="N"
 #echo $file_in
 
 echo_debug () {
-if [[ $debug_mode == "Y" ]]; then
+if [[ "$debug_mode" == "Y" ]]; then
   echo "$1";
 fi;
 }
@@ -19,18 +19,18 @@ else
 fi;
 
 if [[ -f "$file_in" ]]; then
-  count=0;
-  countf=0;
+  _count_=0;
+  _countf_=0;
   echo "file_in:[$file_in]";
   cat $file_in | while read -r the_line
   do
-    ((count++);
+    ((count++));
     if [[ ${the_line:0:2} == "S:" ]]; then _str_=$(echo "${the_line:2}" | tr -d "\r"); echo_debug "str:[$_str_]"; fi;
     if [[ ${the_line:0:2} == "R:" ]]; then _rpl_=$(echo "${the_line:2}" | tr -d "\r"); echo_debug "rpl:[$_rpl_]"; fi;
     if [[ ${the_line:0:2} == "F:" ]]; then _fil_=$(echo "${the_line:2}" | tr -d "\r"); echo_debug "fil:[$_fil_]"; fi;
     if [[ "$_fil_" != "" ]]; then
       if [[ -f "$_fil_" ]]; then
-        ((countf++);
+        ((countf++));
         echo_debug "sed -i \"s|$_str_|$_rpl_|\" $_fil_";
         sed -i "s|$_str_|$_rpl_|" $_fil_;
         _str_n_=$(grep "$_str_" "$_fil_");
@@ -39,7 +39,7 @@ if [[ -f "$file_in" ]]; then
         else
           echo "NOTOK: [CNT:$count, CNTF:$countf]";
         fi;
-        if [[ $debug_mode == "Y" ]]; then
+        if [[ "$debug_mode" == "Y" ]]; then
           echo "grep 1";
           grep "$_str_" "$_fil_";
           echo "grep 2";
@@ -52,6 +52,8 @@ if [[ -f "$file_in" ]]; then
       _rpl_="";
       _fil_="";
     fi;
+    # status=$(echo "[CNT:$count, CNTF:$countf]");
   done;
-  echo "CNT:[$count], CNTF:[$countf]";
+  # echo "$status"
+  # not working outside the while loop (runs in subshell process, do not have access to main shell)
 fi;
