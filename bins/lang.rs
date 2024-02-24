335780a29c88059a1a6b2f4628991af83f3c97ae
deep-soft <deep-soft@users.noreply.github.com>
2024-02-24 02:05:42 +0200

    Create lang.rs

---
+++ bins/lang.rs
@@ -0,0 +1,93 @@
+use hbb_common::regex::Regex;
+use std::ops::Deref;
+
+mod en;
+
+pub const LANGS: &[(&str, &str)] = &[
+    ("en", "English"),
+];
+
+#[cfg(not(any(target_os = "android", target_os = "ios")))]
+pub fn translate(name: String) -> String {
+    let locale = sys_locale::get_locale().unwrap_or_default().to_lowercase();
+    translate_locale(name, &locale)
+}
+
+pub fn translate_locale(name: String, locale: &str) -> String {
+    let mut lang = hbb_common::config::LocalConfig::get_option("lang").to_lowercase();
+    if lang.is_empty() {
+        lang = locale
+            .split("-")
+            .next()
+            .map(|x| x.split("_").next().unwrap_or_default())
+            .unwrap_or_default()
+            .to_owned();
+    }
+    let lang = lang.to_lowercase();
+    let m = match lang.as_str() {
+        _ => en::T.deref(),
+    };
+    let (name, placeholder_value) = extract_placeholder(&name);
+    let replace = |s: &&str| {
+        let mut s = s.to_string();
+        if let Some(value) = placeholder_value.as_ref() {
+            s = s.replace("{}", &value);
+        }
+        s
+    };
+    if let Some(v) = m.get(&name as &str) {
+        if v.is_empty() {
+            if lang != "en" {
+                if let Some(v) = en::T.get(&name as &str) {
+                    return replace(v);
+                }
+            }
+        } else {
+            return replace(v);
+        }
+    }
+    replace(&name.as_str())
+}
+
+// Matching pattern is {}
+// Write {value} in the UI and {} in the translation file
+//
+// Example:
+// Write in the UI: translate("There are {24} hours in a day")
+// Write in the translation file: ("There are {} hours in a day", "{} hours make up a day")
+fn extract_placeholder(input: &str) -> (String, Option<String>) {
+    if let Ok(re) = Regex::new(r#"\{(.*?)\}"#) {
+        if let Some(captures) = re.captures(input) {
+            if let Some(inner_match) = captures.get(1) {
+                let name = re.replace(input, "{}").to_string();
+                let value = inner_match.as_str().to_string();
+                return (name, Some(value));
+            }
+        }
+    }
+    (input.to_string(), None)
+}
+
+mod test {
+    #[test]
+    fn test_extract_placeholders() {
+        use super::extract_placeholder as f;
+
+        assert_eq!(f(""), ("".to_string(), None));
+        assert_eq!(
+            f("{3} sessions"),
+            ("{} sessions".to_string(), Some("3".to_string()))
+        );
+        assert_eq!(f(" } { "), (" } { ".to_string(), None));
+        // Allow empty value
+        assert_eq!(
+            f("{} sessions"),
+            ("{} sessions".to_string(), Some("".to_string()))
+        );
+        // Match only the first one
+        assert_eq!(
+            f("{2} times {4} makes {8}"),
+            ("{} times {4} makes {8}".to_string(), Some("2".to_string()))
+        );
+    }
+}
